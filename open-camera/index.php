75e322bcc446a95fba51904044147c105d674e8f
halfawi1703 <halfawi1703@gmail.com>
2023-09-16 13:32:10 +0700

    add trigger close camera

--- open-camera/index.php
+++ open-camera/index.php
@@ -7,22 +7,38 @@
 </head>
 <body>
     <button id="startCameraButton">Buka Kamera</button>
+    <button id="stopCameraButton" disabled>Tutup Kamera</button>
     <video id="videoElement" autoplay></video>
 
     <script>
         const startCameraButton = document.getElementById('startCameraButton');
+        const stopCameraButton = document.getElementById('stopCameraButton');
         const videoElement = document.getElementById('videoElement');
+        let stream;
 
         startCameraButton.addEventListener('click', () => {
             // Meminta izin akses kamera
             navigator.mediaDevices.getUserMedia({ video: true })
-                .then(function(stream) {
+                .then(function(mediaStream) {
+                    stream = mediaStream;
                     // Menggunakan stream (menampilkan video di elemen HTML)
-                    videoElement.srcObject = stream;
+                    videoElement.srcObject = mediaStream;
+                    startCameraButton.disabled = true;
+                    stopCameraButton.disabled = false;
                 })
                 .catch(function(err) {
                     console.error('Gagal mengakses kamera:', err);
                 });
+        });
+
+        stopCameraButton.addEventListener('click', () => {
+            // Menghentikan akses kamera dengan melepaskan stream
+            if (stream) {
+                stream.getTracks().forEach(track => track.stop());
+                videoElement.srcObject = null;
+                startCameraButton.disabled = false;
+                stopCameraButton.disabled = true;
+            }
         });
     </script>
 </body>
